a3e5237c497f7e295de6c0822fbdcca99003081b
Tran Quoc Cuong <trquoccuong@gmail.com>
2016-08-19 16:26:55 +0700

    State Management with Redux

--- app/index.js
+++ app/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import Routes from './config/routes'
+import { Provider } from 'react-redux'
+import configureStore from './stores'
+const store = configureStore()
 
-export default function App() {
+export default function App () {
     return (
-        <Routes />
+        <Provider store={store}>
+            <Routes />
+        </Provider>
     )
 }
